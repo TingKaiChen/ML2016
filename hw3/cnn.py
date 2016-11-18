fc507eb510e0ce32b2e2c72f35bb3c59fbe82d6e
Kai <kai.tkchen@gmail.com>
2016-11-19 07:09:00 +0800

    update

--- hw3/cnn.py
+++ hw3/cnn.py
@@ -7,16 +7,14 @@ from keras.layers import Convolution2D, MaxPooling2D, Flatten
 from keras.utils import np_utils
 from keras.optimizers import Adadelta
 
-### Parameters ###
-
-
 ### Load image ###
 if '-l' not in sys.argv:
-	all_label = pickle.load(open('./data/all_label.p','rb'))
+	path = sys.argv[1]
+	all_label = pickle.load(open(path+'all_label.p','rb'))
 	all_label = np.array(all_label)
-	all_unlabel = pickle.load(open('./data/all_unlabel.p', 'rb'))
+	all_unlabel = pickle.load(open(path+'all_unlabel.p', 'rb'))
 	all_unlabel = np.array(all_unlabel)
-	test = pickle.load(open('./data/test.p', 'rb'))
+	test = pickle.load(open(path+'test.p', 'rb'))
 	test = test['data']
 	test = np.array(test)
 	np.save('np_label', all_label)
@@ -51,86 +49,114 @@ cv_testout1 = cv_out[2::3, :]
 cv_testout2 = cv_out[0::3, :]
 cv_testout3 = cv_out[1::3, :]
 
+unlabelin = np.zeros((45000, 3, 32, 32))
+for i in xrange(45000):
+	unlabelin[i, :, :, :] = all_unlabel[i].reshape((3,32,32))
 
-### Architecture ###
-if '-m' not in sys.argv:
-	# model = Sequential()
-	# model.add(Convolution2D(32,3,3,input_shape=(3,32,32), dim_ordering='th'))
-	# model.add(Activation('relu'))
-	# model.add(MaxPooling2D((2,2), dim_ordering='th'))
-	# model.add(Activation('relu'))
-	# model.add(Flatten())
-	# model.add(Dense(output_dim=500))
-	# model.add(Activation('relu'))
-	# model.add(Dense(output_dim=10))
-	# model.add(Activation('softmax'))
+if '-train' in sys.argv:
+	### Architecture ###
+	if '-m' not in sys.argv:
+		# model = Sequential()
+		# model.add(Convolution2D(32,3,3,input_shape=(3,32,32), dim_ordering='th'))
+		# model.add(Activation('relu'))
+		# model.add(MaxPooling2D((2,2), dim_ordering='th'))
+		# model.add(Activation('relu'))
+		# model.add(Flatten())
+		# model.add(Dense(output_dim=500))
+		# model.add(Activation('relu'))
+		# model.add(Dense(output_dim=10))
+		# model.add(Activation('softmax'))
 
-	model = Sequential()
+		model = Sequential()
 
-	model.add(Convolution2D(32, 3, 3,
-	                        input_shape=(3,32,32), dim_ordering='th'))
-	model.add(Activation('relu'))
-	model.add(Convolution2D(32, 3, 3, dim_ordering='th'))
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='th'))
-	model.add(Dropout(0.25))
+		model.add(Convolution2D(32, 3, 3,
+		                        input_shape=(3,32,32), dim_ordering='th'))
+		model.add(Activation('relu'))
+		model.add(Convolution2D(32, 3, 3, dim_ordering='th'))
+		model.add(Activation('relu'))
+		model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='th'))
+		model.add(Dropout(0.25))
 
-	model.add(Convolution2D(64, 3, 3, dim_ordering='th'))
-	model.add(Activation('relu'))
-	model.add(Convolution2D(64, 3, 3, dim_ordering='th'))
-	model.add(Activation('relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='th'))
-	model.add(Dropout(0.25))
+		model.add(Convolution2D(64, 3, 3, dim_ordering='th'))
+		model.add(Activation('relu'))
+		model.add(Convolution2D(64, 3, 3, dim_ordering='th'))
+		model.add(Activation('relu'))
+		model.add(MaxPooling2D(pool_size=(2, 2), dim_ordering='th'))
+		model.add(Dropout(0.25))
 
-	model.add(Flatten())
-	model.add(Dense(512))
-	model.add(Activation('relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(10))
-	model.add(Activation('softmax'))
+		model.add(Flatten())
+		model.add(Dense(512))
+		model.add(Activation('relu'))
+		model.add(Dropout(0.5))
+		model.add(Dense(10))
+		model.add(Activation('softmax'))
 
-	adad = Adadelta(lr=0.3, rho=0.95)
-	model.compile(optimizer=adad, loss='categorical_crossentropy', metrics=['accuracy'])
+		adad = Adadelta(lr=0.3, rho=0.95)
+		model.compile(optimizer=adad, loss='categorical_crossentropy', metrics=['accuracy'])
 
-	### Train ###
-	print "###### CV 1 ######"
-	model.fit(cv_train1, cv_out1, batch_size=50, nb_epoch=40)
-	score_cv1 = model.evaluate(cv_testin1, cv_testout1)
-	print
-	print "CV1. Loss:", score_cv1[0], "  Accuracy:", score_cv1[1]
-	print "###### CV 2 ######"
-	model.fit(cv_train2, cv_out2, batch_size=50, nb_epoch=40)
-	score_cv2 = model.evaluate(cv_testin2, cv_testout2)
-	print 
-	print "CV2. Loss:", score_cv2[0], "  Accuracy:", score_cv2[1]
-	print "###### CV 3 ######"
-	model.fit(cv_train3, cv_out3, batch_size=50, nb_epoch=40)
-	score_cv3 = model.evaluate(cv_testin3, cv_testout3)
-	print 
-	print "CV3. Loss:", score_cv3[0], "  Accuracy:", score_cv3[1]
-	print 
-	print "Average accuracy: ", (score_cv1[1]+score_cv2[1]+score_cv3[1])/3
-	print
-	print "###### All Data ######"
-	model.fit(cv_train, cv_out, batch_size=50, nb_epoch=40)
+		### Train ###
+		print "###### CV 1 ######"
+		model.fit(cv_train1, cv_out1, batch_size=50, nb_epoch=40)
+		score_cv1 = model.evaluate(cv_testin1, cv_testout1)
+		print
+		print "CV1. Loss:", score_cv1[0], "  Accuracy:", score_cv1[1]
+		print "###### CV 2 ######"
+		model.fit(cv_train2, cv_out2, batch_size=50, nb_epoch=40)
+		score_cv2 = model.evaluate(cv_testin2, cv_testout2)
+		print 
+		print "CV2. Loss:", score_cv2[0], "  Accuracy:", score_cv2[1]
+		print "###### CV 3 ######"
+		model.fit(cv_train3, cv_out3, batch_size=50, nb_epoch=40)
+		score_cv3 = model.evaluate(cv_testin3, cv_testout3)
+		print 
+		print "CV3. Loss:", score_cv3[0], "  Accuracy:", score_cv3[1]
+		print 
+		print "Average accuracy: ", (score_cv1[1]+score_cv2[1]+score_cv3[1])/3
+		print
+		print "###### All Data ######"
+		model.fit(cv_train, cv_out, batch_size=50, nb_epoch=40)
 
-	### Save model ###
-	model.save('my_model.h5')
-else:
-	model = load_model('my_model.h5')
+		### Save model ###
+	# 	model.save('trained_model')
+	# else:
+	# 	model = load_model('trained_model')
 
-### Prediction ###
-testdata = np.zeros((10000, 3, 32, 32))
-for i in xrange(10000):
-	testdata[i, :, :, :] = test[i].reshape((3, 32, 32))
+	### Self-training ###
+	if '-self' in sys.argv:
+		sampleW = np.ones(5000)*10
+		for it in xrange(4):
+			if len(unlabelin) == 0:
+				break
+			unlabelout = model.predict(unlabelin)
+			addList = np.amax(unlabelout, axis=1)>0.9
+			addList = np.where(addList == True)
+			addList = addList[0]
+			print
+			print '# of added unlabel data:', len(addList)
+			print
+			cv_train = np.concatenate((cv_train, unlabelin[addList, :, :, :]))
+			cv_out = np.concatenate((cv_out, (unlabelout[addList, :]>0.9).astype(int)))
+			unlabelin = np.delete(unlabelin, addList, axis=0)
+			unlabelout = np.delete(unlabelout, addList, axis=0)
+			
+			sampleW = np.concatenate((sampleW, np.ones(len(addList))))
+			model.fit(cv_train, cv_out, batch_size=100, nb_epoch=10, sample_weight=sampleW)
+		model.save('trained_model')
 
-testout = model.predict(testdata)
-testout = np.argmax(testout, axis = 1)
-
-with open('prediction.csv', 'w+') as pd:
-	pd.write('ID,class\n')
+if '-test' in sys.argv:
+	model = load_model('trained_model')
+	### Prediction ###
+	testdata = np.zeros((10000, 3, 32, 32))
 	for i in xrange(10000):
-		pd.write(str(i)+','+str(testout[i])+'\n')
+		testdata[i, :, :, :] = test[i].reshape((3, 32, 32))
+
+	testout = model.predict(testdata)
+	testout = np.argmax(testout, axis = 1)
+
+	with open(sys.argv[3], 'w+') as pd:
+		pd.write('ID,class\n')
+		for i in xrange(10000):
+			pd.write(str(i)+','+str(testout[i])+'\n')
 
 
 
