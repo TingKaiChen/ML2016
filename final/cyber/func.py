2b4c1583654357bc240a2c6027c43a045c437bab
Kai <kai.tkchen@gmail.com>
2016-12-24 17:08:00 +0800

    Allen Wu's codes

---
+++ final/cyber/func.py
@@ -0,0 +1,105 @@
+
+import numpy as np
+import csv
+
+nb_train = 4408587
+nb_test = 606779
+dim = 41
+types = {'normal' : 0, 'dos' : 1, 'u2r' : 2, 'r2l' : 3, 'probe' : 4}
+
+def read_train(train_file):
+    
+    X = np.zeros((nb_train, dim))
+    y = np.zeros((nb_train,))
+
+    idx = 0
+    dim_1 = {}
+    dim_2 = {}
+    dim_3 = {}
+    dim_1c = 0.0
+    dim_2c = 0.0
+    dim_3c = 0.0
+
+    maptype = {}
+    maptypec = 0
+    mapclass = read_label()
+
+    with open(train_file, 'r') as f:
+        for line in f:
+            data = line.strip('.\n')
+            data = data.split(',')
+
+            for i in xrange(len(data)-1):
+                if i == 1:
+                    if not data[i] in dim_1:
+                        dim_1[data[i]] = dim_1c
+                        dim_1c += 1.0
+                    X[idx, i] = dim_1[data[i]]
+                elif i == 2:
+                    if not data[i] in dim_2:
+                        dim_2[data[i]] = dim_2c
+                        dim_2c += 1.0
+                    X[idx, i] = dim_2[data[i]]
+                elif i == 3:
+                    if not data[i] in dim_3:
+                        dim_3[data[i]] = dim_3c
+                        dim_3c += 1.0
+                    X[idx, i] = dim_3[data[i]]
+                else:
+                    X[idx, i] = float(data[i])
+
+            '''
+            if not data[-1] in maptype:
+                maptype[data[-1]] = maptypec
+                maptypec += 1
+            '''
+
+            y[idx] = mapclass[data[-1]] #maptype[data[-1]]
+            idx += 1
+
+    return X, y.astype(int), dim_1, dim_2, dim_3, mapclass
+            
+
+def read_test(test_file, dim_1, dim_2, dim_3):
+    
+    X = np.zeros((nb_test, dim))
+    idx = 0
+    with open(test_file, 'r') as f:
+        for line in f:
+            data = line.strip()
+            data = data.split(',')
+            for i in xrange(len(data)):
+                if i == 1:
+                    X[idx, i] = dim_1[data[i]]
+                elif i == 2:
+                    if not data[i] in dim_2:
+                        dim_2[data[i]] = max(dim_2.values())+1
+                    X[idx, i] = dim_2[data[i]]
+                elif i == 3:
+                    X[idx, i] = dim_3[data[i]]
+                else:
+                    X[idx, i] = float(data[i])
+
+            idx += 1
+
+    return X
+
+def read_label():
+    
+    mapclass = {'normal':0}
+
+    with open('training_attack_types.txt', 'r') as f:
+        for line in f:
+            data = line.strip()
+            data = data.split()
+            mapclass[data[0]] = types[data[1]]
+    return mapclass
+
+def write_pred(pred_file, pred):
+    with open(pred_file, 'wb') as f:
+        wr = csv.writer(f)
+        wr.writerow(["id", "label"])
+        for i in xrange(len(pred)):
+            wr.writerow([i+1, int(pred[i])])
+
+
---
+++ final/cyber/randfor.py
@@ -0,0 +1,85 @@
+
+from func import *
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import TruncatedSVD, PCA
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+from sklearn.ensemble import GradientBoostingClassifier
+'''
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.utils import np_utils
+from keras.layers.advanced_activations import ELU
+
+import tensorflow
+from tensorflow.python.ops import control_flow_ops 
+tensorflow.python.control_flow_ops = control_flow_ops
+'''
+
+print 'Reading Data'
+train_file = 'train'
+test_file = 'test.in'
+output = 'rf.csv'
+
+X, y, dim_1, dim_2, dim_3, maptype = read_train(train_file)
+
+
+X_test = read_test(test_file, dim_1, dim_2, dim_3)
+
+#pca = PCA(n_components=30)
+
+#X = np.concatenate((X_train, X_test))
+#X = pca.fit_transform(X)
+
+#X_train, y_train = X[:3526869], y[:3526869]
+#X_valid, y_valid = X[3526869:], y[3526869:]
+
+#X = X[:, :40]
+#X_test = X_test[:, :40]
+
+print 'Training'
+#X = SelectKBest(chi2, k=35).fit_transform(X, y)
+#clf = GradientBoostingClassifier(n_estimators=25, learning_rate=1.0, random_state=0)
+clf = RandomForestClassifier(n_estimators=25, criterion='gini', max_features=20, warm_start=True)
+
+scores = cross_val_score(clf, X, y, cv=5)
+print scores
+print np.mean(scores)
+
+clf.fit(X, y)
+print clf.score(X, y)
+#print clf.score(X_valid, y_valid)
+
+print 'Predicting'
+pred = clf.predict(X_test)
+write_pred(output, pred)
+
+'''
+batch_size = 1024
+nb_epoch = 20
+
+# the data, shuffled and split between train and test sets
+
+
+# convert class vectors to binary class matrices
+Y = np_utils.to_categorical(y, len(types))
+
+model = Sequential()
+model.add(Dense(512, input_shape=(dim,)))
+model.add(ELU())
+#model.add(Dropout(0.2))
+model.add(Dense(512))
+model.add(ELU())
+#model.add(Activation('relu'))
+#model.add(Dropout(0.2))
+model.add(Dense(len(types)))
+model.add(Activation('softmax'))
+
+
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.1), metrics=['accuracy'])
+
+history = model.fit(X, Y, batch_size=batch_size, nb_epoch=nb_epoch, validation_split=0.2)
+'''
