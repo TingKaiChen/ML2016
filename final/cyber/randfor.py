ef3587437c0f6edaf2e1ec614c39b4c40032da1e
Kai <kai.tkchen@gmail.com>
2017-01-19 15:28:05 +0800

    cv test

--- final/cyber/randfor.py
+++ final/cyber/randfor.py
@@ -43,23 +43,23 @@ X_test = read_test(test_file, dim_1, dim_2, dim_3)
 print 'Training'
 #X = SelectKBest(chi2, k=35).fit_transform(X, y)
 #clf = GradientBoostingClassifier(n_estimators=25, learning_rate=1.0, random_state=0)
-clf = RandomForestClassifier(n_estimators=25, criterion='gini', max_features=20, warm_start=True)
+clf = RandomForestClassifier(n_estimators=25, criterion='gini', max_features=40, warm_start=True)
 
 scores = cross_val_score(clf, X, y, cv=5)
 print scores
 print np.mean(scores)
 
-clf.fit(X, y)
-print clf.score(X, y)
-#print clf.score(X_valid, y_valid)
+# clf.fit(X, y)
+# print clf.score(X, y)
+# #print clf.score(X_valid, y_valid)
 
-print 'Predicting'
-pred = clf.predict(X_test)
-prob = clf.predict_proba(X_test)
-for i in xrange(len(pred)):
-	if prob[i,2] != 0.0:
-		pred[i] = 2
-write_pred(output, pred)
+# print 'Predicting'
+# pred = clf.predict(X_test)
+# prob = clf.predict_proba(X_test)
+# for i in xrange(len(pred)):
+# 	if prob[i,2] != 0.0:
+# 		pred[i] = 2
+# write_pred(output, pred)
 
 '''
 batch_size = 1024
